ff981d3775a08c4540e2b4bda000b3c088f644ab
Adrian Yang <69127002+adrianyang2012@users.noreply.github.com>
2023-08-30 20:22:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ for (var i = 0; i < names.length; i++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
+  console.log(names[i].charAt(1).toLowerCase);
   if (names[i].charAt(1).toLowerCase === 'j') {
     speak_hi.say_hi();
   } else {
