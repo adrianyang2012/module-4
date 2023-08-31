6e3fbd177467adc55970fbe4c92751740dd7e7ea
Adrian Yang <69127002+adrianyang2012@users.noreply.github.com>
2023-08-30 20:17:37 -0700

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,9 +1,10 @@
 
 (function (window) {
 var speakWord = "Hello";
-var store_speak_hi = {};
-store_speak_hi.say_hi = (function (name) {
+var speak_hi = {};
+speak_hi.say_hi = (function (name) {
   console.log(hello.speakWord + " " + name);
 })
+window.speak_hi = speak_hi;
 })(window)
 
