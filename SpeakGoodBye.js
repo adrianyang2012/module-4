201f4081f9fa8ce25490612d8b3c0fc4218143b8
Adrian Yang <69127002+adrianyang2012@users.noreply.github.com>
2023-08-30 20:33:33 -0700

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -3,7 +3,7 @@
 var speakWord = "Good Bye";
 var speak_bye = {};
 speak_bye.say_good_bye = (function (name) {
-  console.log(hello.speakWord + " " + name);
+  console.log(speakWord + " " + name);
 })
 window.speak_bye = speak_bye;
 })(window)
