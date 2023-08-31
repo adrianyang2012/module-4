8d7c58cd5fb8835a51643ab1fbc4f1b61d04c208
Adrian Yang <69127002+adrianyang2012@users.noreply.github.com>
2023-08-30 20:08:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (names[i].charAt(0).toLowerCase === 'j') {
-    speak(names[i]);
+    hello.speak(names[i]);
   } else {
     speak(names[i]);
   }
