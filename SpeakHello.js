003627fbf904896563d7974197f532970e54be03
Adrian Yang <69127002+adrianyang2012@users.noreply.github.com>
2023-08-30 20:02:48 -0700

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -17,6 +17,7 @@ var speakWord = "Hello";
 function speak(name) {
   console.log(speakWord + " " + name);
 }
+window.speakWord = speakWord
 }(window)
 
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
